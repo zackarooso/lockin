fe26aaca896d5abf91ec2bba3203851758e6d967
zackarooso <129443619+zackarooso@users.noreply.github.com>
2026-04-18 16:20:46 -0700

    fix: clean title, add Google Font link tags to head

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=DM+Sans:wght@300;400;500;600;700&family=Permanent+Marker&display=swap" rel="stylesheet" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <meta name="theme-color" content="#07030F" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
